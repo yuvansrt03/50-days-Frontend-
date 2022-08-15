d19e76dc73f4ab2e14c837194ea0123395c77917
Yuvan <yuviyuvan15@gmail.com>
2022-08-15 20:30:25 +0530

    Delete Day4 directory

--- Day4/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('.btn')
-const rel = document.querySelector('.rel')
-const inp = document.querySelector('.input')
-btn.addEventListener('click', () => {
-    btn.classList.toggle('rel')
-    inp.classList.toggle('active')
-})
